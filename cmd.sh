ac47fbada4830fa6b5627fb49a1f8b1b78f554a3
Belhanafi Abdelmadjid <112507960+BelhanafiAbdelmadjid@users.noreply.github.com>
2024-05-18 19:42:55 +0100

    cleaning from venv

---
+++ cmd.sh
@@ -0,0 +1,6 @@
+python - 'argument1' 'argument2'
+
+-python : le mot clé pour l'ouverture d'une console
+- "-" : indique que la console reçoit des arguments
+- 'argument1' 'argument2' : liste des arguments passer a la console
+#dans notre cas ce sont les inputs utilisateurs
